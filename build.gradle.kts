05f920e781300a95cb8066daabc58f02d5578da5
1 <386029724@qq.com>
2025-08-30 09:46:02 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.compose.compiler) apply false
+    id("com.android.application") version "8.10.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
 }
 buildscript {
     repositories {
@@ -14,8 +14,6 @@ buildscript {
     }
     dependencies {
 
-
-        classpath(libs.google.services)
-
+        classpath("com.google.gms:google-services:4.4.3")
     }
 }
