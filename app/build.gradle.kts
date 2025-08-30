c0207636570978391509630fdf529e7f160e59ca
1 <386029724@qq.com>
2025-08-30 09:07:54 +0800

    remove libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ import com.android.build.gradle.internal.api.BaseVariantOutputImpl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.compose.compiler)
+    id("com.android.application") version "8.10.1"
+    id("org.jetbrains.kotlin.android") version "2.2.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
     id("com.google.gms.google-services")
     id("kotlin-parcelize")
     kotlin("plugin.serialization") version "2.2.0"
@@ -70,34 +70,38 @@ android {
 
 dependencies {
 
-    implementation(libs.mixfile.core)
-    implementation(libs.androidx.material.icons.extended)
-    implementation(libs.mmkv)
-    implementation(libs.zoomable)
-    implementation(libs.coil)
-    implementation(libs.coil.compose)
-    implementation(libs.coil.gif)
-    implementation(libs.coil.svg)
-    implementation(libs.coil.video)
-    implementation(libs.androidx.media3.exoplayer) // [Required] androidx.media3 ExoPlayer dependency
-    implementation(libs.androidx.media3.session) // [Required] MediaSession Extension dependency
-    implementation(libs.androidx.media3.ui) // [Required] Base Player UI
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.firebase.analytics)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.appcompat)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.github.InvertGeek:mixfile-core:2.0.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("com.tencent:mmkv:1.3.14")
+    implementation("net.engawapg.lib:zoomable:1.6.1")
+    implementation("io.coil-kt:coil:2.6.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("io.coil-kt:coil-gif:2.6.0")
+    implementation("io.coil-kt:coil-svg:2.6.0")
+    implementation("io.coil-kt:coil-video:2.6.0")
+
+    implementation("androidx.media3:media3-exoplayer:1.8.0") // [Required] androidx.media3 ExoPlayer dependency
+    implementation("androidx.media3:media3-session:1.8.0") // [Required] MediaSession Extension dependency
+    implementation("androidx.media3:media3-ui:1.8.0") // [Required] Base Player UI
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+    implementation("com.google.firebase:firebase-analytics:23.0.0")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation(platform("androidx.compose:compose-bom:2024.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
