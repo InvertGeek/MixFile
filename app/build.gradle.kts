e3f5d654527f90df0a2e29b92803351173edefd9
1 <386029724@qq.com>
2026-05-03 10:41:49 +0800

    2.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.donut.mixfile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 157
-        versionName = "2.0.9.1"
+        versionCode = 158
+        versionName = "2.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -72,7 +72,7 @@ dependencies {
     // 引用 libs 目录下的所有 aar
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-    implementation("com.github.InvertGeek:mixfile-core:2.0.8")
+    implementation("com.github.InvertGeek:mixfile-core:2.0.10")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("com.tencent:mmkv:1.3.14")
     implementation("net.engawapg.lib:zoomable:1.6.1")
